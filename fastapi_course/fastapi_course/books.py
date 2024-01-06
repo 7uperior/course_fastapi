375e693d63e0de9d260b378afe3879c4e565416e
Vadim Surin <7uperior@gmail.com>
2024-01-06 09:56:50 +0300

    Add general course code

--- fastapi_course/fastapi_course/books.py
+++ fastapi_course/fastapi_course/books.py
@@ -3,6 +3,25 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+BOOKS = {
+    "book_1": {"title": "title_1", "author": "author_1"},
+    "book_2": {"title": "title_2", "author": "author_2"},
+    "book_3": {"title": "title_3", "author": "author_3"},
+    "book_4": {"title": "title_4", "author": "author_4"},
+    "book_5": {"title": "title_5", "author": "author_5"},
+}
+
+
 @app.get("/")
-async def first_api():
-    return {"message": "Welcome, Vadim!"}
+async def read_all_books():
+    return BOOKS
+
+
+@app.get("/books/mybook")
+async def read_favorite_book():
+    return {"book_title": "My Favorite Book"}
+
+
+@app.get("/books/{boook_id}")
+async def read_book(book_id: int):
+    return {"book_titile": book_id}
