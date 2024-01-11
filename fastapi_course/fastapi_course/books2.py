d2bacd7b1cd3bc4011f08f72efbd2d4af5e91756
Vadim Surin <7uperior@gmail.com>
2024-01-11 15:10:33 +0300

    Add class Config and json_schema_extra

--- fastapi_course/fastapi_course/books2.py
+++ fastapi_course/fastapi_course/books2.py
@@ -1,7 +1,7 @@
 from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
-from uuid import UUID
+from uuid import UUID, uuid4
 
 app = FastAPI()
 
@@ -18,12 +18,25 @@ class Book(BaseModel):
     )
     rating: int = Field(gt=-1, lt=101)
 
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "id": uuid4(),
+                "title": "Vadim is the Best PRO Python Programmer",
+                "author": "Vadim Surin",
+                "description": "Vadim will do all the things needed to be a PRO Python Coder",
+                "rating": 100,
+            }
+        }
+
 
 BOOKS = []
 
 
 @app.get("/")
 async def read_all_books():
+    if len(BOOKS) < 1:
+        create_books_no_api()
     return BOOKS
 
 
@@ -31,3 +44,38 @@ async def read_all_books():
 async def create_book(book: Book):
     BOOKS.append(book)
     return book
+
+
+def create_books_no_api():
+    book_1 = Book(
+        id=uuid4(),
+        title="Title 1",
+        author="Author 1",
+        description="Description 1",
+        rating=60,
+    )
+    book_2 = Book(
+        id=uuid4(),
+        title="Title 2",
+        author="Author 2",
+        description="Description 2",
+        rating=70,
+    )
+    book_3 = Book(
+        id=uuid4(),
+        title="Title 3",
+        author="Author 3",
+        description="Description 3",
+        rating=80,
+    )
+    book_4 = Book(
+        id=uuid4(),
+        title="Title 4",
+        author="Author 4",
+        description="Description 4",
+        rating=90,
+    )
+    BOOKS.append(book_1)
+    BOOKS.append(book_2)
+    BOOKS.append(book_3)
+    BOOKS.append(book_4)
