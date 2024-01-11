f4558e95f82d41cf6a3cf3150ca64a7a3271c47e
Vadim Surin <7uperior@gmail.com>
2024-01-11 16:13:11 +0300

    Add Delete request

--- fastapi_course/fastapi_course/books2.py
+++ fastapi_course/fastapi_course/books2.py
@@ -72,6 +72,17 @@ async def update_book(book_id: UUID, book: Book):
             return BOOKS[counter - 1]
 
 
+@app.delete("/{book_id}")
+async def delete_book(book_id: UUID):
+    counter = 0
+
+    for x in BOOKS:
+        counter += 1
+        if x.id == book_id:
+            del BOOKS[counter - 1]
+            return f"book with ID: {book_id} deleted"
+
+
 def create_books_no_api():
     book_1 = Book(
         id=uuid4(),
